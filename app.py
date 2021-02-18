66db5ff1a56cb2f165ac8b4609b4a252079409e4
Seif Wu <seif.wuxf@gmail.com>
2021-02-18 17:02:49 +0800

    feat: add index page

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask, escape
+from flask import Flask, escape, render_template
 
 app = Flask(__name__)
+
+my_name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 
 @app.route('/index')
 @app.route('/home')
 @app.route('/')
 def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    return render_template('index.html', name=my_name, movies=movies)
 
 
 @app.route('/user/<name>')
